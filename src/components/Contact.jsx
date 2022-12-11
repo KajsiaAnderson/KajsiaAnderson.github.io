a3c1826271503bfb8958d0442925243d321425c4
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-12-11 16:04:47 -0700

    updated styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,11 +4,11 @@ import styles from './Contact.module.css'
 const Contact = () => {
     return (
         <div className={styles.contact_section}>
-            <h1 className={styles.title}>Check out my stuff... </h1>
             <div className={styles.links}>
-                <a href="https://www.linkedin.com/in/kajsiaanderson">linkedin</a>
-                <p>||</p>
-                <a href="https://github.com/KajsiaAnderson">github</a>
+                <h1 className={styles.title}> Check out my stuff at
+                    <a href="https://www.linkedin.com/in/kajsiaanderson"> linkedin</a> or
+                    <a href="https://github.com/KajsiaAnderson"> github</a>.
+                </h1>
             </div>
         </div>
     )
